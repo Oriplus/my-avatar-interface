14e744b81151bf35703ef6c435f1d31fb9da89cf
ocmv <orianacmv@gmail.com>
2022-08-13 18:51:07 -0400

    styles(project): add files

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -58,7 +58,7 @@ const MainLayout = ({ children }) => {
           />
           <HStack spacing={8} alignItems={"center"}>
             <Flex alignItems="center">
-              <Image src="./images/platzi.svg" width="80px" />
+              <Image src="./images/my-avatar-logo.png" width="80px" />
               <Heading size="md" color="purple" mt={0.2} ml={1}>
                 MyAvatars
               </Heading>
